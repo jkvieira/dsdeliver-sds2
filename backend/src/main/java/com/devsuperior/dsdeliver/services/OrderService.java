ad5ea1d4f33fa9f6929a29ecd60e1f29cac66825
unknown <jkvieira@inf.ufpel.edu.br>
2021-01-08 20:22:27 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,15 +9,35 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
-	@Transactional(readOnly = true)
+	@Autowired
+	private ProductRepository productRepository;
+
+    
+	@Transactional
+	 public OrderDTO insert(OrderDTO dto){
+		 Order order= new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+	     //inserir pedidos com os produtos
+		 for(ProductDTO p: dto.getProducts()) {
+				 Product product= productRepository.getOne(p.getId());
+				 order.getProducts().add(product);
+		 }
+		 order = repository.save(order);
+		 return new OrderDTO(order);
+	}
+
+	@Transactional()
 	 public List<OrderDTO> findAll(){
 		 List<Order> list= repository.findOrdersWithProducts();
 		 return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());

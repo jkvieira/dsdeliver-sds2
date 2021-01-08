c7671d70a45fd3b9a1d72e219c3dd41f37cf2324
unknown <jkvieira@inf.ufpel.edu.br>
2021-01-08 20:45:46 -0300

    PuT /orders status

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -25,6 +25,12 @@ public class OrderService {
 	private ProductRepository productRepository;
 
     
+	@Transactional()
+	 public List<OrderDTO> findAll(){
+		 List<Order> list= repository.findOrdersWithProducts();
+		 return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+	
 	@Transactional
 	 public OrderDTO insert(OrderDTO dto){
 		 Order order= new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
@@ -37,10 +43,12 @@ public class OrderService {
 		 return new OrderDTO(order);
 	}
 
-	@Transactional()
-	 public List<OrderDTO> findAll(){
-		 List<Order> list= repository.findOrdersWithProducts();
-		 return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
-	 }
+	@Transactional
+	 public OrderDTO setDelivered(Long id){
+		Order order= repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order= repository.save(order);
+		return new OrderDTO(order);	
+	}
 
 }
